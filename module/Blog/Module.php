7f58bed5c2fc01576ceda1c7079caf509627a88c
khanhdeux <khanhdeux@gmail.com>
2015-11-21 11:38:38 +0000

    first commit

---
+++ module/Blog/Module.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Blog;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+{
+
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    // Autoload all classes from namespace 'Blog' from '/module/Blog/src/Blog'
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                )
+            )
+        );
+
+    }
+
+}
---
+++ module/Blog/src/Blog/Service/PostService.php
@@ -0,0 +1,80 @@
+<?php
+
+// Filename: /module/Blog/src/Blog/Service/PostService.php
+namespace Blog\Service;
+
+use Blog\Model\Post;
+
+class PostService implements PostServiceInterface
+{
+
+    protected $data = array(
+
+        array(
+            'id'    => 1,
+            'title' => 'Hello World #1',
+            'text'  => 'This is our first blog post!'
+        ),
+        array(
+            'id'     => 2,
+            'title' => 'Hello World #2',
+            'text'  => 'This is our second blog post!'
+        ),
+        array(
+            'id'     => 3,
+            'title' => 'Hello World #3',
+            'text'  => 'This is our third blog post!'
+        ),
+        array(
+            'id'     => 4,
+            'title' => 'Hello World #4',
+            'text'  => 'This is our fourth blog post!'
+        ),
+        array(
+            'id'     => 5,
+            'title' => 'Hello World #5',
+            'text'  => 'This is our fifth blog post!'
+        )
+
+    );
+
+    /**
+     * Should return a set of all blog posts that we can iterate over. Single entries of the array are supposed to be
+     * implementing \Blog\Model\PostInterface
+     *
+     * @return array|PostInterface[]
+     */
+    public function findAllPosts()
+    {
+
+        $allPosts = array();
+
+        foreach($this->data as $index => $post) {
+            $allPosts[] = $this->findPost($index);
+        }
+
+        return $allPosts;
+
+    }
+
+    /**
+     * Should return a single blog post
+     *
+     * @param  int $id Identifier of the Post that should be returned
+     * @return PostInterface
+     */
+    public function findPost($id)
+    {
+
+        $postData = $this->data[$id];
+
+        $model = new Post();
+        $model->setId($postData['id']);
+        $model->setTitle($postData['title']);
+        $model->setText($postData['text']);
+
+        return $model;
+
+    }
+
+}
